f54892587b7f2acae157f84149346b5cb95b322f
piemasters <pie_masters@hotmail.co.uk>
2014-08-20 15:14:20 +0100

    First university setup.

---
+++ src/General/HelloWorld.java
@@ -0,0 +1,10 @@
+package General;
+
+/**
+ * Created by David on 20/08/2014.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
