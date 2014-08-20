7cb593c88ae0d41dc251210607324cc0905ad1a2
piemasters <pie_masters@hotmail.co.uk>
2014-08-20 15:17:48 +0100

    First university setup.

--- src/General/HelloWorld.java
+++ src/General/HelloWorld.java
@@ -6,5 +6,6 @@ package General;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!"); // Display the string.
     }
 }
